8a474c748377ce338ab48ba6cb10427ea0cf7653
Jae Lee <jaequery@gmail.com>
2011-10-09 15:32:37 -0700

    JIEN-5 initial db wrapper

--- application/models/DbTable/User.php
+++ application/models/DbTable/User.php
@@ -3,5 +3,12 @@
 class Application_Model_DbTable_User extends My_Model
 {
     protected $_name = 'User';
-       
+    protected $_alias = 'u';
+ 	protected $_soft_delete = true;
+
+ 	public function byHuman(){
+ 		$this->andWhere("u.gender = 'f' OR u.gender = 'm'");
+ 		return $this;
+ 	}
+ 	
 }
--- library/Jien/Model.php
+++ library/Jien/Model.php
@@ -2,6 +2,7 @@
 
 class Jien_Model extends Zend_Db_Table_Abstract {
 
+	
 	public function init(array $config = array()){
   	
 		// parent constructor
@@ -17,42 +18,18 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		// set masterdb resource
 		$this->masterdb = Zend_Registry::get('masterdb');
     
-	}
-  
-	/**
-	 * takes an array of data and filters out any data not relevant to current table schema
-	 *
-	 * @param array $arr
-	 * @return array
-	 */
-	public function filterCols($arr){
-		$data = array();
-		$schema = $this->info();
-		foreach($arr AS $key=>$value){
-			if(in_array($key, $schema['cols'])){
-				$data[$key] = $value;
-			}
-		}
-		return $data;
+		// set default alias
+		if(empty($this->_alias)) $this->_alias = $this->_name;
+		$this->_primary = $this->getPrimary();
 	}
 	
-	public function getById($id, $bypass_cache = false){
+	public function getById($id){
 		
-    	$info = $this->info();
-		$primary = $info['primary'][1];
-		$tablename = $this->_name;
-		
-		$id = (int)$id;
-		
-		$sql = 'SELECT * FROM ' . $tablename . ' WHERE ' . $primary . ' = ' . $id;
-		
-		if ($bypass_cache) {
-			$sql .= ' AND ' . time() . ' > 0';
-		}
-		
-        $row = $this->db->fetchRow($sql);
+		$row = $this->find($id);
         if (!$row) {
-            throw new Exception("Could not find $primary: $id");
+            throw new Exception("Could not find {$this->getPrimary()}: $id");
+        }else{
+        	$row = $row->current();
         }
         return $row;
 	}
@@ -60,7 +37,7 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 	public function save($data){
 		
 		$info = $this->info();
-		$primary = $info['primary'][1];
+		$primary = $this->getPrimary();
 		
 		// if 'id' is passed, consider this as primary key
 		if(!empty($data['id'])){
@@ -78,19 +55,19 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		
 		// if editing, just update
 		if(!empty($data[$primary])){
-			$cond = "$primary = $data[$primary]";
-			unset($data['created']);
-			$data['updated'] = Jien::getDateTime(time());
-			$affected = $this->update($data, $cond);
+			$affected = $this->update($data, "{$primary} = {$data[$primary]}");
 			return $data[$primary];
 		}else{
 			// create a new record
+			$data['active'] = 1;
 			return $this->insert($data);	
 		}
 		
 	}
 	
 	public function update(array $data, $where){
+		unset($data['created']);
+		$data['updated'] = new Zend_Db_Expr('NOW()');
 		return $this->masterdb->update($this->_name, $data, $where);
 	}
 	
@@ -100,17 +77,183 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		return $id;
 	}
 	
-	public function delete($where){
-		return $this->masterdb->delete($this->_name, $where);
+	public function getPrimary(){
+		$info = $this->info();
+		return $info['primary'][1];
 	}
 	
-	public function __call($method, $args) {
-    	$method = substr($method,1);
-    	if(!empty($args[0])){
-    		$this->data[$method] = $args[0];
-    	}
-    	return $this;
-    }
+	public function delete($where){
+		if(is_int($where)){
+			$where = $this->getPrimary() . " = " . $where;
+		}
+		if($this->_soft_delete === true){
+			return $this->update(array(
+				"deleted"	=>	new Zend_Db_Expr('NOW()'),
+				"active"	=>	0,
+			), $where);
+		}else{
+			return $this->masterdb->delete($this->_name, $where);
+		}
+		
+	}
 
+	protected function _resetQuery(){
+		$this->_query = array();
+	}
+	
+	protected function _getQuery(){
+		$where = '';
+		$select = array();
+		
+		$from = array($this->_alias=>$this->_name);
+		
+		if($this->_soft_delete){
+			$this->andWhere("{$this->_alias}.active = 1");
+		}
+		
+		if(!empty($this->_query['where']['and'])){
+			$where = '('. implode(' AND ', $this->_query['where']['and']) . ')';
+		}
+		if(!empty($this->_query['where']['or'])){
+			$where = $where . ' OR ' . implode(' OR ', $this->_query['where']['or']);
+		}
+		
+		if(!empty($this->_query['select'])){
+			foreach($this->_query['select'] AS $k=>$v){
+				$fields = explode(",", $v);
+				foreach($fields AS $field){
+					array_push($select, trim($field));
+				}
+			}
+		}
+		
+		// start query
+		$q = Jien::db()->select();
+		
+		if(!empty($select)){
+			$q = $q->from($from, $select);
+		}else{
+			$q = $q->from($from);
+		}
+		
+		if(!empty($where)){
+			$q = $q->where($where);
+		}
+		
+		if(!empty($this->_query['group'])){
+			$group = array();
+			foreach($this->_query['group'] AS $k=>$v){
+				$fields = explode(",", $v);
+				foreach($fields AS $field){
+					array_push($group, trim($field));
+				}
+			}
+			$q->group($group);
+		}
+		
+		if(!empty($this->_query['having'])){
+			$having = array();
+			foreach($this->_query['having'] AS $k=>$v){
+				$fields = explode(",", $v);
+				foreach($fields AS $field){
+					array_push($having, trim($field));
+				}
+			}
+			$q->having($having);
+		}
+		
+		if(!empty($this->_query['order'])){
+			$order = array();
+			foreach($this->_query['order'] AS $k=>$v){
+				$fields = explode(",", $v);
+				foreach($fields AS $field){
+					array_push($order, trim($field));
+				}
+			}
+			$q->order($order);
+		}
+		
+		if(!empty($this->_query['limit'])){
+			$q->limit($this->_query['limit']['limit'], $this->_query['limit']['skip']);
+		}
+		
+		return $q;
+	}
+	
+	public function get($where = ''){
+		if($where){
+			if(is_numeric($where)){
+				$this->andWhere("{$this->_alias}.{$this->getPrimary()} = {$where}");
+			}
+		}
+		$res = $this->getAll($where);
+		if(!empty($res[0])){
+			return $res[0];
+		}else{
+			return false;
+		}
+	}
+	
+	public function getAll($where = ''){
+		if($where){
+			$this->andWhere($where);
+		}
+		$select = $this->_getQuery();
+		$stmt = Jien::db()->query($select);
+		$rows = $stmt->fetchAll();
+		$res = array();
+		if($rows){
+			$res = $rows;
+			$this->_resetQuery();
+		}
+		return $res;
+	}
+
+	public function groupBy($group){
+		$this->_query['group'][] = $group;
+		return $this;
+	}
+	
+	public function having($having){
+		$this->_query['having'][] = $having;
+		return $this;
+	}
+	
+	public function select($select = ''){
+		if($select) $this->_query['select'] = array($select);
+		return $this;
+	}
+	
+	public function addSelect($select){
+		$this->_query['select'][] = $select;
+		return $this;
+	}
+	
+	public function where($where){
+		$this->_query['where']['and'] = array('('.$where.')');
+		return $this;
+	}
+	
+	public function andWhere($where){
+		$this->_query['where']['and'][] = '('.$where.')';
+		return $this;
+	}
+	
+	public function orWhere($where){
+		$this->_query['where']['or'][] = '('.$where.')';
+		return $this;
+	}
+	
+	public function orderBy($order){
+		$this->_query['order'][] = $order;
+		return $this;
+	}
+
+	public function limit($limit, $skip = 0){
+		$this->_query['limit']['limit'] = $limit;
+		$this->_query['limit']['skip'] = $skip;
+		return $this;
+	}
+	
 }
 ?>
