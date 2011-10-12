e1766ca672a6f9bc33a5bca4ae25b9a3cef366e3
Jae <jaequery@gmail.com>
2011-10-12 00:06:17 -0700

    JIEN-22 Setting up Hello World landing page

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,7 +7,7 @@ class IndexController extends My_Controller {
     }
     
     public function indexAction(){
-		$this->view->data = Jien::model("User")->enablePager($this->_getParam('page', 1), 50)->getAll();
+		
     }
     
 }
