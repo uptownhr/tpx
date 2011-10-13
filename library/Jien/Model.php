8280f2ca2db75d7bfec2471b0a61115703870f08
Jae Lee <jaequery@gmail.com>
2011-10-12 20:34:13 -0700

    test whitespace

--- library/Jien/Model.php
+++ library/Jien/Model.php
@@ -2,46 +2,46 @@
 
 class Jien_Model extends Zend_Db_Table_Abstract {
 
-	
+
 	public function init(array $config = array()){
-  	
+
 		// parent constructor
 		parent::init($config);
-		
+
 		// get default db and set it to this object
 		$this->db = Zend_Registry::get('db');
-		
+
 		// set cache
 		$cache = Zend_Registry::get('cache');
 		Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
-		
+
 		// set masterdb resource
 		$this->masterdb = Zend_Registry::get('masterdb');
-    
+
 		// set default alias
 		if(empty($this->_alias)) $this->_alias = $this->_name;
 		$this->_primary = $this->getPrimary();
 	}
-	
+
 	public function save($data, $where = ''){
-		
+
 		$info = $this->info();
 		$primary = $this->getPrimary();
-		
+
 		// if 'id' is passed, consider this as primary key
 		if(!empty($data['id'])){
 			$id = $data['id'];
 			unset($data['id']);
 			$data[$primary] = $id;
 		}
-		
+
 		// filter out any data not in table columns
 		foreach($data AS $field=>$value){
 			if(!in_array($field, $info['cols'])){
 				unset($data[$field]);
 			}
 		}
-		
+
 		// if editing, just update
 		if($where){
 			return $this->update($data, "{$where}");
@@ -49,18 +49,18 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 			return $this->update($data, "{$primary} = {$data[$primary]}");
 		}else{
 			// create a new record
-			$id = $this->insert($data);	
+			$id = $this->insert($data);
 			return $id;
 		}
-		
+
 	}
-	
+
 	public function update(array $data, $where){
 		unset($data['created']);
 		$data['updated'] = new Zend_Db_Expr('NOW()');
 		return $this->masterdb->update($this->_name, $data, $where);
 	}
-	
+
 	public function insert(array $data){
 		$data['active'] = 1;
 		$data['created'] = new Zend_Db_Expr('NOW()');
@@ -68,14 +68,14 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		$id = $this->masterdb->lastInsertId();
 		return $id;
 	}
-	
+
 	public function getPrimary(){
 		$info = $this->info();
 		return $info['primary'][1];
 	}
-	
+
 	public function delete($where){
-		
+
 		if(is_numeric($where)){
 			$where = $this->getPrimary() . " = " . $where;
 		}
@@ -87,30 +87,30 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		}else{
 			return $this->masterdb->delete($this->_name, $where);
 		}
-		
+
 	}
 
 	protected function _resetQuery(){
 		$this->_query = array();
 	}
-	
+
 	protected function _getQuery(){
 		$where = '';
 		$select = array();
-		
+
 		$from = array($this->_alias=>$this->_name);
-		
+
 		if($this->_soft_delete){
 			$this->andWhere("{$this->_alias}.active = 1");
 		}
-		
+
 		if(!empty($this->_query['where']['and'])){
 			$where = '('. implode(' AND ', $this->_query['where']['and']) . ')';
 		}
 		if(!empty($this->_query['where']['or'])){
 			$where = $where . ' OR ' . implode(' OR ', $this->_query['where']['or']);
 		}
-		
+
 		if(!empty($this->_query['select'])){
 			foreach($this->_query['select'] AS $k=>$v){
 				$fields = explode(",", $v);
@@ -119,20 +119,20 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 				}
 			}
 		}
-		
+
 		// start query
 		$q = Jien::db()->select();
-		
+
 		if(!empty($select)){
 			$q = $q->from($from, $select);
 		}else{
 			$q = $q->from($from);
 		}
-		
+
 		if(!empty($where)){
 			$q = $q->where($where);
 		}
-		
+
 		if(!empty($this->_query['group'])){
 			$group = array();
 			foreach($this->_query['group'] AS $k=>$v){
@@ -143,7 +143,7 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 			}
 			$q->group($group);
 		}
-		
+
 		if(!empty($this->_query['having'])){
 			$having = array();
 			foreach($this->_query['having'] AS $k=>$v){
@@ -154,7 +154,7 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 			}
 			$q->having(implode(" AND ", $having));
 		}
-		
+
 		if(!empty($this->_query['order'])){
 			$order = array();
 			foreach($this->_query['order'] AS $k=>$v){
@@ -165,16 +165,16 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 			}
 			$q->order($order);
 		}
-		
+
 		if(!empty($this->_query['limit'])){
 			$q->limit($this->_query['limit']['limit'], $this->_query['limit']['skip']);
 		}
-		
+
 		if(!empty($this->_query['join'])){
 			foreach($this->_query['join'] AS $table=>$v){
 				$join_alias = $v['alias'];
 				$join_method = 'join' . ucfirst($v['type']);
-				
+
 				if(!empty($v['fields'])){
 					$join_fields = array();
 					$xJoinFields = explode(",", $v['fields']);
@@ -186,13 +186,13 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 				}else{
 					$q->$join_method(array($join_alias => $table), $v['condi']);
 				}
-				
+
 			}
 		}
-		
+
 		return $q;
 	}
-	
+
 	public function get($id = ''){
 		if($id && is_numeric($id)){
 			$this->andWhere("{$this->_alias}.{$this->getPrimary()} = {$id}");
@@ -204,7 +204,7 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 			return false;
 		}
 	}
-	
+
 	public function enablePager($current_page = 1, $item_count_per_page = 10, $page_range = 10){
 		$this->_query['pager'] = array(
 			"current_page"	=>	$current_page,
@@ -214,20 +214,21 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		$this->limit($item_count_per_page, ($current_page - 1) * $item_count_per_page);
 		return $this;
 	}
-	
+
 	public function getAll($where = ''){
 		if($where){
 			$this->andWhere($where);
 		}
 		$select = $this->_getQuery();
-		
+
+		// whitespace test
 		if(!empty($this->_query['pager'])){
 			$pager = Zend_Paginator::factory($select);
 	        $pager->setCurrentPageNumber($this->_query['pager']['current_page']);
 	        $pager->setItemCountPerPage($this->_query['pager']['item_count_per_page']);
 	        $pager->setPageRange($this->_query['pager']['page_range']);
 		}
-		
+
 		$stmt = Jien::db()->query($select);
 		$rows = $stmt->fetchAll();
 		$res = array();
@@ -247,37 +248,37 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		$this->_query['group'][] = $group;
 		return $this;
 	}
-	
+
 	public function having($having){
 		$this->_query['having'][] = $having;
 		return $this;
 	}
-	
+
 	public function select($select = ''){
 		if($select) $this->_query['select'] = array($select);
 		return $this;
 	}
-	
+
 	public function addSelect($select){
 		$this->_query['select'][] = $select;
 		return $this;
 	}
-	
+
 	public function where($where){
 		$this->_query['where']['and'] = array('('.$where.')');
 		return $this;
 	}
-	
+
 	public function andWhere($where){
 		$this->_query['where']['and'][] = '('.$where.')';
 		return $this;
 	}
-	
+
 	public function orWhere($where){
 		$this->_query['where']['or'][] = '('.$where.')';
 		return $this;
 	}
-	
+
 	public function orderBy($order){
 		$this->_query['order'][] = $order;
 		return $this;
@@ -288,7 +289,7 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		$this->_query['limit']['skip'] = $skip;
 		return $this;
 	}
-	
+
 	public function join($type, $table, $condi, $fields = ''){
 		$xTable = explode(" ", $table);
 		$table = $xTable[0];
@@ -301,41 +302,41 @@ class Jien_Model extends Zend_Db_Table_Abstract {
 		$this->_query['join'][$table]['type'] = 'left';
 		$this->_query['join'][$table]['condi'] = $condi;
 		$this->_query['join'][$table]['fields'] = $fields;
-		
+
 		return $this;
 	}
-	
+
 	public function leftJoin($table, $condi, $fields = ''){
 		$this->join('left', $table, $condi, $fields);
 		return $this;
 	}
-	
+
 	public function innerJoin($table, $condi, $fields = ''){
 		$this->join('inner', $table, $condi, $fields);
 		return $this;
 	}
-	
+
 	public function rightJoin($table, $condi, $fields = ''){
 		$this->join('right', $table, $condi, $fields);
 		return $this;
 	}
-	
+
 	public function fullJoin($table, $condi, $fields = ''){
 		$this->join('full', $table, $condi, $fields);
 		return $this;
 	}
-	
+
 	public function scheme(){
  		$info = $this->info();
  		$meta = $info['metadata'];
  		$scheme = $meta;
  		return $scheme;
  	}
-	
+
 	public function withUser(){
     	$this->leftJoin("User", "u.user_id = {$this->_alias}.user_id");
     	return $this;
     }
-    
+
 }
 ?>
